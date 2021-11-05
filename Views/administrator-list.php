49a25d178e1d3d0995e40c13d5d0a9c449c59c6f
pinku_protogen <66691079+mercadoMatias@users.noreply.github.com>
2021-11-04 22:03:49 -0300

    No password in administratorList

--- Views/administrator-list.php
+++ Views/administrator-list.php
@@ -8,15 +8,13 @@
         <tr id="tableIndex">
                 <td style='max-width: 10px;'>ID</td>
                 <td>UserName</td>
-                <td>Password</td>
                 <td><button class="btn btn-primary" onclick="window.location.href='<?php echo FRONT_ROOT ?>Administrator/AddView'">Add</button>
         </tr>
         <?php
         foreach($administratorList as $eachAdministrator){
                 echo "<tr>
                         <td style='max-width: 10px; text-align: center;'>".$eachAdministrator->getAdministratorId()."</td>
-                        <td style='max-width: 100px;'>".$eachAdministrator->getUserName()."</td>
-                        <td style='max-width: 230px;'>".$eachAdministrator->getPassword()."</td>"; ?>
+                        <td style='max-width: 100px;'>".$eachAdministrator->getUserName()."</td>"; ?>
                         <td></td>                    
                 </tr>
         <?php } ?>
