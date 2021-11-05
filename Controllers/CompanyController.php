d0ee52bd80299f4d90a4d551be997ff057960a08
María Lucía Cuenca <66698971+mlc91@users.noreply.github.com>
2021-11-05 11:39:05 -0300

    Required fields in company - Changes in companycontroller

--- Controllers/CompanyController.php
+++ Controllers/CompanyController.php
@@ -100,47 +100,6 @@
             $this->ShowListView();
         }
 
-        private function showCompany($company) {
-            ?>
-            <tr>
-              <td><?php echo $company->getName() ?></td>
-              <td><?php echo $company->getCuit() ?></td>
-              <td><?php echo $company->getDescription() ?></td>
-              <td><a href="<?php echo $company->getWebsite() ?>"><?php echo $company->getWebsite() ?></a></td>
-              <td><?php echo $company->getStreet()." ".$company->getNumber() ?></td>
-              <td style="align='right'"><button class="btn btn-danger" onclick="window.location.href='<?php echo FRONT_ROOT ?>Company/ViewDetail?companyId=<?php echo $company->getCompanyId() ?>'">Details</button></td>
-            <?php
-                if($this->isAdmin()) {
-                ?>
-                    <td><button class="btn btn-danger" onclick="window.location.href='<?php echo FRONT_ROOT ?>Company/Remove?removeId=<?php echo $company->getCompanyId() ?>'">Remove</button></td>
-                    <td><button class="btn btn-danger" onclick="window.location.href='<?php echo FRONT_ROOT ?>Company/ModifyView?modifyId=<?php echo $company->getCompanyId() ?>'">Edit</button></td>
-                <?php
-                }
-                ?>
-              </tr>
-            <?php
-        } 
-
-        public function companyFilter($searchedCompany, $companyList) {
-            $i = 0;
-            if($searchedCompany != ""){
-                foreach($companyList as $company){
-                    if(strpos(strtolower($company->getName()), strtolower($searchedCompany)) !== false && $company->getActive() == 1){
-                        $i++;
-                        $this->showCompany($company);
-                    }
-                }
-            }else{
-                foreach($companyList as $company){
-                    if($company->getActive() == 1){
-                        $i++;
-                        $this->showCompany($company);
-                    }     
-                }   
-            }
-            echo "<br><b>There are ".$i." Result/s!</b>";
-        }
-
         public function ViewDetail($companyId) {
             $company = $this->companyDAO->FindById($companyId);
 
--- Views/add-company.php
+++ Views/add-company.php
@@ -11,49 +11,49 @@
           <th colspan="2"><center><h4>Add Company</h4></center></th>
         </tr>
         <tr>
-          <td style="width: 200px;"><label for="name">Name</label></td> 
+          <td style="width: 200px;"><label for="name">Name<abbr title="required" aria-label="required">*</abbr></label></td> 
           <td>
             <input type="text" name="name" required>
           </td>
         </tr>  
         <tr>
-          <td style="width: 200px;"><label for="cuit">CUIT</label></td> 
+          <td style="width: 200px;"><label for="cuit">CUIT (without hyphen)<abbr title="required" aria-label="required">*</abbr></label></td> 
           <td>
             <input max="999999999999" type="number" name="cuit" required>
           </td>
         </tr> 
         <tr>
-          <td style="width: 200px;"><label for="description">Description</label></td> 
+          <td style="width: 200px;"><label for="description">Description<abbr title="required" aria-label="required">*</abbr></label></td> 
           <td>
             <input type="text" name="description" required>
           </td>
         </tr>   
         <tr>
-          <td style="width: 200px;"><label for="website">Website</label></td> 
+          <td style="width: 200px;"><label for="website">Website<abbr title="required" aria-label="required">*</abbr></label></td> 
           <td>
             <input type="text" name="website" required>
           </td>
         </tr>  
         <tr>
-          <td style="width: 200px;"><label for="street">Street</label></td> 
+          <td style="width: 200px;"><label for="street">Street<abbr title="required" aria-label="required">*</abbr></label></td> 
           <td>
             <input type="text" name="street" required>
           </td>
         </tr>
         <tr>
-          <td style="width: 200px;"><label for="number_street">Number</label></td> 
+          <td style="width: 200px;"><label for="number_street">Number<abbr title="required" aria-label="required">*</abbr></label></td> 
           <td>
             <input type="number" name="number_street" required>
           </td>
         </tr>
         <tr>
-          <td style="width: 200px;"><label for="aboutUs">About Us</label></td> 
+          <td style="width: 200px;"><label for="aboutUs">About Us<abbr title="required" aria-label="required">*</abbr></label></td> 
           <td>
             <input type="text" name="aboutUs" required>
           </td>
         </tr>   
         <tr>
-          <td style="width: 200px;"><label for="industry">Industry</label></td> 
+          <td style="width: 200px;"><label for="industry">Industry<abbr title="required" aria-label="required">*</abbr></label></td> 
           <td>
               <select name="industry">  
                 <?php
@@ -65,7 +65,7 @@
           </td>
         </tr>  
         <tr>
-          <td style="width: 200px;"><label for="active">Active</label></td> 
+          <td style="width: 200px;"><label for="active">Active<abbr title="required" aria-label="required">*</abbr></label></td> 
           <td>
             <input type="radio" name="isActive" value="1" checked required>Active
             <input type="radio" name="isActive" value="0" required>Not active
